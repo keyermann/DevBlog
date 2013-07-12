d124c0146868867e29b5856ceb068461de950d51
keyermann <kevin.eyermann@ign.fr>
2013-07-12 17:02:05 +0200

    on fait correspondre l'entité à l'existant

--- src/SGN/DevBlogBundle/Entity/User.php
+++ src/SGN/DevBlogBundle/Entity/User.php
@@ -17,10 +17,140 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="text", nullable=true)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="text", nullable=true)
+     */
+    private $prenom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="initiales", type="text", nullable=true)
+     */
+    private $initiales;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="categorie", type="text", nullable=true)
+     */
+    private $categorie;
 
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     * @return User
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     * @return User
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string 
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set initiales
+     *
+     * @param string $initiales
+     * @return User
+     */
+    public function setInitiales($initiales)
+    {
+        $this->initiales = $initiales;
+
+        return $this;
+    }
+
+    /**
+     * Get initiales
+     *
+     * @return string 
+     */
+    public function getInitiales()
+    {
+        return $this->initiales;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set categorie
+     *
+     * @param string $categorie
+     * @return User
+     */
+    public function setCategorie($categorie)
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    /**
+     * Get categorie
+     *
+     * @return string 
+     */
+    public function getCategorie()
+    {
+        return $this->categorie;
     }
 }
